43d53c51193b69ff3e08ab677d794009508fbccb
ShoSeshake <sho122seshake@gmail.com>
2019-09-16 11:43:05 +0900

    dalete time-format
    time format seems to be causing some issue when  building up server via unicorn.rb

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module ChatSpace
   class Application < Rails::Application
     config.i18n.default_locale = :ja
     config.action_view.automatically_disable_submit_tag = false
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local 
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
